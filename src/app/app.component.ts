f8f9b62dd4b5478d6bbdcd5919ce0cb56bef5c16
Gideon Bamuleseyo <gideonbamuleseyo@gmail.com>
2019-12-30 00:38:47 +0300

    Add Prettier for code formatting

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
-
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  template: `
+    <h1>{{ title }}</h1>
+  `
 })
 export class AppComponent {
-  title = 'shophere';
+  title = 'Shop Here';
 }
