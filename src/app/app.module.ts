d91ded2cdde9524a2f0cc690c7023ffab32d6720
Gideon Bamuleseyo <gideonbamuleseyo@gmail.com>
2019-12-31 15:19:39 +0300

    Add custom pipe

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,16 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ProductListComponent } from './products/product-list.component';
+import { ConvertToSpacesPipe } from './shared/convert-to-spaces.pipe';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    ProductListComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent, ProductListComponent, ConvertToSpacesPipe],
+  imports: [BrowserModule, AppRoutingModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
--- src/app/products/product-list.component.ts
+++ src/app/products/product-list.component.ts
@@ -1,9 +1,55 @@
 import { Component } from '@angular/core';
+import { IProduct } from './product';
 
 @Component({
   selector: 'shophere-products',
-  templateUrl: './product-list.component.html'
+  templateUrl: './product-list.component.html',
+  styleUrls: ['./product-list.component.css']
 })
 export class ProductListComponent {
   pageTitle: string = 'Product List';
+  imageWidth: number = 50;
+  imageMargin: number = 2;
+  showImage: boolean = false;
+  listFilter: string = 'cart';
+  products: IProduct[] = [
+    {
+      productId: 100,
+      productName: 'Loafer',
+      starRating: 4,
+      productCode: 'xyz-120',
+      releaseDate: '12-01-2019',
+      price: 12300,
+      description: 'Some dope product',
+      imageUrl:
+        'https://images.unsplash.com/photo-1576792741377-eb0f4f6d1a47?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80'
+    },
+    {
+      productId: 100,
+      productName: 'Loafer',
+      starRating: 4,
+      productCode: 'xyz-120',
+      releaseDate: '12-01-2019',
+      price: 12300,
+      description: 'Some dope product',
+      imageUrl:
+        'https://images.unsplash.com/photo-1576792741377-eb0f4f6d1a47?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80'
+    },
+    {
+      productId: 100,
+      productName: 'Loafer',
+      starRating: 4,
+      productCode: 'xyz-120',
+      releaseDate: '12-01-2019',
+      price: 12300,
+      description: 'Some dope product',
+      imageUrl:
+        'https://images.unsplash.com/photo-1576792741377-eb0f4f6d1a47?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80'
+    }
+  ];
+
+  toggleImage(): void {
+    this.showImage = !this.showImage;
+    console.log('Toggle Image Button Clicked');
+  }
 }
